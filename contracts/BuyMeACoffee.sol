d318ecd3db8b40e77e63465ccf04a3fb2a53f035
Vishruth Devan <vishruthdevan@gmail.com>
2022-10-22 20:07:46 +0530

    feat: create struct and event for creating memos

---
+++ contracts/BuyMeACoffee.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.0;
+
+contract BuyMeACoffee {
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message        
+    );
+
+    struct Memo{
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+}
