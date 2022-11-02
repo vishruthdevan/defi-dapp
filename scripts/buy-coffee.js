462edefb644f6b49801bffa8f9c41cfe746fde05
Vishruth Devan <vishruthdevan@gmail.com>
2022-11-02 23:12:33 +0530

    chore: helper functions to print memo information

--- scripts/buy-coffee.js
+++ scripts/buy-coffee.js
@@ -19,9 +19,19 @@ async function printBalances(addresses) {
   }
 }
 
-async function main() {
-
+async function printMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(
+      `At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`
+    );
+  }
 }
+
+async function main() {}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
